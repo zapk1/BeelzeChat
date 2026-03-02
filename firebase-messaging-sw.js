aaf63051d4cbf0bae4d722889df6eefb47977d42
DMS <darkmasterstore@gmail.com>
2026-03-02 13:37:14 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+    apiKey: 'AIzaSyBfimOu2e0xAMoRyM-rwNMgAk9G_Hgt558',
+    authDomain: 'blackrituals.firebaseapp.com',
+    projectId: 'blackrituals',
+    storageBucket: 'blackrituals.firebasestorage.app',
+    messagingSenderId: '511685923038',
+    appId: '1:511685923038:web:4db052df64ebb1e194724f'
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: 'https://i.postimg.cc/mD0dbHM0/1000287857.png',
+        badge: 'https://i.postimg.cc/mD0dbHM0/1000287857.png',
+        vibrate: [200, 100, 200],
+        data: payload.data,
+        actions: [{ action: 'reply', title: 'Reply' }]
+    });
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/'));
+});
